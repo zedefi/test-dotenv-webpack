7b9b4220ce1dbdd900f5e60cec27ca39fcf52f44
Filip Zedek <filip.zedek@compacer.com>
2020-10-03 09:51:21 +0200

    First and last commit

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const env = process.env;
+ 
+console.log('ENV', process.env)
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  entry: './src/index.ts',
+  target: 'node',
+  
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.mjs'],
+  },
+  module: {
+  
+    rules: [
+      {
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX: 'development'
+    }),
+    new Dotenv({
+    })
+  ]
+}
